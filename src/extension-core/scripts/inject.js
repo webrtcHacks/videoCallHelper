be2ae34efa85b446f3762db021cd96fe0097c925
Chad Hart <chadwhart@gmail.com>
2023-04-10 07:29:39 -0400

    impairment worker rework
    had to modify Impairment class; packetloss not working

--- src/extension-core/scripts/inject.js
+++ src/extension-core/scripts/inject.js
@@ -38,7 +38,7 @@ async function transferStream(stream){
         return;
 
     return new Promise((resolve, reject) => {
-        const timeOut = 1000;                               // 1 second timeout
+        const timeOut = 1500;                               // 1.5 second timeout
         const startTime = new Date().getTime();             // measure time to return new stream
 
         // ToDo: shadow dom?
