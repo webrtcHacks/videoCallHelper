0b1e489a459b72b35ec70d248efbfc7529b03d63
Chad Hart <chadwhart@gmail.com>
2023-04-21 18:07:31 -0400

    add build time to webpack dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,20 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const path = require("path");
 
+//
+class BuildTimePlugin {
+    apply(compiler) {
+        compiler.hooks.done.tap('BuildTimePlugin', (compilation) => {
+            setTimeout(() => {
+                const date = new Date();
+                date.setMilliseconds(date.getMilliseconds() - 500);
+                const buildTime = date.toLocaleTimeString();
+                console.log(`Build completed at ${buildTime}`);
+            }, 500);
+        });
+    }
+}
+
 module.exports = merge(commonConfig, {
     mode: 'development',
     devtool: 'inline-source-map',
@@ -11,4 +25,5 @@ module.exports = merge(commonConfig, {
             path.resolve(__dirname, 'dist/dev/scripts'),
         clean: true
     },
+    plugins: [new BuildTimePlugin()]
 });
