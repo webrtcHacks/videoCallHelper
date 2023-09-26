6f09200c248a42cc15643d3caed8323a10ce3f41
Chad Hart <chadwhart@gmail.com>
2023-09-26 08:20:53 -0400

    exclude menu UI

--- src/dash/dash.js
+++ src/dash/dash.js
@@ -193,6 +193,8 @@ const dmEnabledCheck = document.querySelector("input#dm_enabled_check");
 const audioMenu = document.getElementById('audio_devices_list');
 const videoMenu = document.getElementById('video_devices_list');
 const excludeMenu = document.getElementById('all_devices_list');
+const excludeDropdown = document.getElementById('excludeDropdown');
+
 // ToDo: guidance text
 
 dmEnabledCheck.checked = storage.contents['deviceManager']['enabled'] || false;
@@ -209,72 +211,281 @@ dmEnabledCheck.onclick = async () => {
 // Finding: device enumeration permissions are not shared with the extension context
 //  only default devices are returned
 
-async function populateDeviceDropdowns(devices) {
+async function populateDeviceDropdowns() {
 
     // Clear previous options
     audioMenu.innerHTML = '';
     videoMenu.innerHTML = '';
     excludeMenu.innerHTML = '';
 
-    try {
-        // const devices = await navigator.mediaDevices.enumerateDevices();
-        debug(devices);
-        devices.forEach((device) => {
+    const devices = storage.contents['deviceManager']['currentDevices'] || [];
+
+    // Sort devices - needed for exclude menu
+    // ToDo: check spec - is this needed? Does enumerateDevices always sort?
+    devices.sort((a, b) => {
+        const order = ['videoinput', 'audioinput', 'audiooutput'];
+        return order.indexOf(a.kind) - order.indexOf(b.kind);
+    });
+    debug("sorted devices: ", devices);
+
+    // Reconstruct static items for excludeMenu
+
+    // Guide Note
+    const note = document.createElement('li');
+    note.className = 'dropdown-header fw-bold';
+    note.innerText = 'Unselect items to exclude';
+    excludeMenu.appendChild(note);
+
+    // Divider
+    const divider2 = document.createElement('li');
+    divider2.innerHTML = '<hr class="dropdown-divider">';
+    excludeMenu.appendChild(divider2);
+
+    // Utility function to add segment label and divider for exclude menu
+    function addSegmentLabelAndDivider(kind) {
+        let label;
+        switch (kind) {
+            case 'videoinput':
+                label = 'Video Inputs';
+                break;
+            case 'audioinput':
+                label = 'Audio Inputs';
+                break;
+            case 'audiooutput':
+                label = 'Audio Outputs';
+                break;
+        }
+
+        const flexContainer = document.createElement('li');
+        flexContainer.className = 'd-flex justify-content-between align-items-center px-3 pt-2';
+
+        const header = document.createElement('span');
+        header.className = 'dropdown-header';
+        header.innerText = label;
+        flexContainer.appendChild(header);
+
+        const applyButton = document.createElement('button');
+        applyButton.className = 'applyExclusions btn btn-primary btn-sm';
+        applyButton.innerText = 'Apply All';
+        flexContainer.appendChild(applyButton);
+
+        excludeMenu.appendChild(flexContainer);
+
+        const divider = document.createElement('li');
+        divider.className = 'dropdown-divider';
+        excludeMenu.appendChild(divider);
+    }
+
+    let lastKind = null;
+
+    // populate the dropdowns
+    devices.forEach((device) => {
+
+        /*** START common menu item processing ***/
 
             // improve the label text in the case where permissions don't provide labels
-            let betterLabel = device.label;
-            if(device.label === ""){
-                let betterKind = device.kind
-                    .replace('videoinput', 'camera')
-                    .replace('audioinput', 'microphone')
-                    .replace('audiooutput', 'speaker');
-                betterLabel = `default ${betterKind}`;
-            }
+        let betterLabel = device.label;
+        if (device.label === "") {
+            let betterKind = device.kind
+                .replace('video input', 'camera')
+                .replace('audio input', 'microphone')
+                .replace('audio output', 'speaker');
+            betterLabel = `default ${betterKind}`;
+        }
 
-            let option = document.createElement('li');
-            option.className = 'dropdown-item';
-            option.textContent = betterLabel;
-            option.dataset.deviceId = device.deviceId;
+        const option = document.createElement('li');
+        option.className = 'dropdown-item device-dropdown';
 
-            if (device.kind === 'audioinput') {
-                audioMenu.appendChild(option);
-            } else if (device.kind === 'videoinput') {
-                videoMenu.appendChild(option);
-            }
+        let textNode = document.createTextNode(betterLabel);
+        option.appendChild(textNode);
 
-            // Add option to Exclude Devices dropdown
-            // Future: update selected items in dropdown
-            excludeMenu.appendChild(option.cloneNode(true));
-        });
-    } catch (error) {
-        console.error('Error enumerating devices:', error);
-    }
+        option.dataset.deviceId = device.deviceId;
+
+        /*** END common menu item processing ***/
+
+            // make a copy for the exclude Menu
+        const excludeMenuOption = option.cloneNode(true)
+
+        /**** START audio & video menus ****/
+            // Create the span for the checkmark & prepend it
+        let checkmarkSpan = document.createElement('span');
+        checkmarkSpan.className = 'bi bi-check';
+        option.prepend(checkmarkSpan);  // Add the checkmark to the option
+
+        if (device.kind === 'audioinput') {
+            const selectedAudioDevice = storage.contents?.deviceManager?.selectedDeviceLabels?.audio;
+            if (device.label === selectedAudioDevice || device.label === selectedAudioDevice)
+                option.classList.add('selected');
+            audioMenu.appendChild(option);
+        } else if (device.kind === 'videoinput') {
+            const selectedVideoDevice = storage.contents?.deviceManager?.selectedDeviceLabels?.video;
+            if (device.label === selectedVideoDevice || device.label === selectedVideoDevice)
+                option.classList.add('selected');
+            videoMenu.appendChild(option);
+        }
+        /**** END audio & video menus ****/
+
+        /**** START exclude menu ****/
+
+        // Check if device kind changed
+        if (lastKind !== device.kind) {
+            addSegmentLabelAndDivider(device.kind);
+            lastKind = device.kind;
+        }
+
+        // Add checkmark
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'me-2'; // Bootstrap's margin-end
+        // ToDo: logic to tell if it should be checked
+        checkbox.checked = true;
+        excludeMenuOption.prepend(checkbox);
+
+        // Does this need to be cloned?
+        excludeMenu.appendChild(excludeMenuOption);
+        /**** END exclude menu ****/
+    });
 }
 
 // Initial population
-const devices = storage.contents['deviceManager']['currentDevices'] || [];
-await populateDeviceDropdowns(devices);
+await populateDeviceDropdowns()
+    .catch(err => {
+        console.error('Error populating  device dropdowns:', err);
+    });
 
 // ToDo: handle permissions changes
 // ToDo: handle if devices array is empty
 storage.addListener('deviceManager', async (newValue) => {
     dmEnabledCheck.checked = storage.contents['deviceManager']['enabled'] || false;
 
-    if(newValue.currentDevices)
+    if (newValue.currentDevices)
         await populateDeviceDropdowns(newValue.currentDevices);
 
 });
 
+// ToDo: Make the selected item visible if the scroll hides it - only useful for long lists
+function scrollToElement(container, element) {
+    // Check if the element is out of view
+    const containerTop = container.scrollTop;
+    const containerBottom = containerTop + container.clientHeight;
+    const elemTop = element.offsetTop;
+    const elemBottom = elemTop + element.clientHeight;
+
+    if (elemTop < containerTop) {
+        // If the element is above the current view
+        container.scrollTop = elemTop;
+    } else if (elemBottom > containerBottom) {
+        // If the element is below the current view
+        container.scrollTop = elemBottom - container.clientHeight;
+    }
+}
+
+
+audioMenu.addEventListener('click', async (e) => {
+
+    // if(storage.contents?.deviceManager?.selectedDeviceLabels?.audio)
+
+    // Check if a dropdown item was clicked
+    if (e.target.classList.contains('device-dropdown')) {
+        // Remove the 'selected' class from all items in this dropdown
+        audioMenu.querySelectorAll('.device-dropdown').forEach(item => {
+            item.classList.remove('selected');
+        });
+
+        const label = e.target.textContent;
+        // Add the 'selected' class to the clicked item
+        e.target.classList.add('selected');
+        const selectedDeviceLabels = {
+            audio: label,
+            video: storage.contents?.deviceManager?.selectedDeviceLabels?.video
+        }
+        await storage.update("deviceManager", {selectedDeviceLabels});
+
+
+        // Log the selected device
+        debug(`Selected audio device: ${label}`);
+    }
+});
+
+videoMenu.addEventListener('click', async (e) => {
+
+    // Check if a dropdown item was clicked
+    if (e.target.classList.contains('device-dropdown')) {
+        // Remove the 'selected' class from all items in this dropdown
+        videoMenu.querySelectorAll('.device-dropdown').forEach(item => {
+            item.classList.remove('selected');
+        });
+
+        const label = e.target.textContent;
+        // Add the 'selected' class to the clicked item
+        e.target.classList.add('selected');
+        const selectedDeviceLabels = {
+            audio: storage.contents?.deviceManager?.selectedDeviceLabels?.audio,
+            video: label
+        }
+        await storage.update("deviceManager", {selectedDeviceLabels});
+
+        // Log the selected device
+        debug(`Selected video device: ${e.target.textContent}`);
+    }
+});
+
+// Exclude Devices dropdown UI listeners
+excludeDropdown.addEventListener('click', () => {
+    // Always scroll to the top to see the note
+    excludeMenu.scrollTop = 0;
+});
+
+// apply exclude menu changes
+function handleExcludeMenuClose() {
+    const allItems = document.querySelectorAll('#all_devices_list .device-dropdown');
+
+    const excludedDevices = [];
+    const includedDevices = [];
+
+    allItems.forEach(item => {
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        const deviceId = item.dataset.deviceId;
+        if (!checkbox.checked) {
+            excludedDevices.push(deviceId);
+        } else {
+            includedDevices.push(deviceId);
+        }
+    });
+
+    debug('Excluded devices:', excludedDevices);
+    debug('Included devices:', includedDevices);
+}
+
+// document.querySelector('.dropup').addEventListener('hide.bs.dropdown'
+// document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
+    let excludeMenu = document.getElementById('exclude_devices_menu');
+
+    let isClickInsideMenu = excludeMenu.contains(event.target);
+    let isClickOnDropdownBtn = excludeDropdown.contains(event.target);
+
+    // If the click was neither inside the menu nor the dropdown button, handle it
+    if (!isClickInsideMenu && !isClickOnDropdownBtn) {
+        handleExcludeMenuClose();
+    }
+});
+
+document.querySelectorAll('.applyExclusions').forEach(
+    btn => btn.addEventListener('click', handleExcludeMenuClose)
+);
 
 // Optionally, update when devices are added/removed
-navigator.mediaDevices.ondevicechange = () => async () =>{
-    debug("device change detected - populate dropdowns here");
+navigator.mediaDevices.ondevicechange = () => async () => {
+    debug("device change detected - should I populate dropdowns here?");
     // await populateDeviceDropdowns();
+
+    // rely on the target page monitoring this event and doing a enumerateDevices update for now
+    //  otherwise I would need to ask inject to call enumerateDevices manually
 };
 
 
 /************ END deviceManager ************/
-
 
 
 /************ START badConnection ************/
@@ -293,7 +504,7 @@ const bcsSelect = document.querySelector("input#bcs_level");
 
 // Permanently disable the bad connection simulator if there is no peer connection
 // -- no longer needed with fake device approach
-function disableBcs(){
+function disableBcs() {
     debug("peerConnection not open in time - disabling bad connection simulator");
     const bcsDiv = document.querySelector("div#bcs");
     let childElements = bcsDiv.getElementsByTagName('*');
