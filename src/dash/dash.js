a8074c9d07bbd841c63524f157c6faccac51910b
Chad Hart <chadwhart@gmail.com>
2024-04-18 15:14:49 -0700

    dash refactor for presence

--- src/dash/dash.js
+++ src/dash/dash.js
@@ -17,54 +17,9 @@ window.vch = {
 /**
  * Applet script imports
  */
+import '../presence/scripts/dash.mjs';
 import '../imageCapture/scripts/dash.mjs';
 import '../deviceManager/scripts/dash.mjs';
-
-/************ START presence ************/
-// backend functions
-import {webRequest} from "../presence/scripts/webRequest.mjs";
-
-const statusSpanElem = document.querySelector('span#presence_status');
-const enabledCheck = document.querySelector('input#enable_presence_check');
-const btnBusy = document.querySelector('button#busy');
-const btnNotBusy = document.querySelector('button#not_busy');
-
-// initial state
-statusSpanElem.innerText = storage.contents?.presence?.active ? "active" : "inactive";
-enabledCheck.checked = storage.contents?.presence?.enabled;
-
-document.querySelector("button#presence_setup").onclick = async () => {
-    const url = chrome.runtime.getURL("pages/presence.html");
-    await chrome.tabs.create({url});
-}
-
-enabledCheck.onclick = async () => {
-    const enabled = enabledCheck.checked;
-    debug(`presence enabled set to ${enabled}`);
-    await storage.update('presence', {enabled: enabled});
-}
-
-
-btnBusy.onclick = async () => {
-    await storage.update('presence', {active: true});
-    webRequest("on", storage.contents['presence']);
-    // ToDo: handle embrava
-};
-
-btnNotBusy.onclick = async () => {
-    await storage.update('presence', {active: false});
-    webRequest("off", storage.contents['presence']);
-    // ToDo: handle embrava
-}
-
-storage.addListener('presence', (newValue) => {
-    debug("presence changed", newValue);
-    statusSpanElem.innerText = newValue.active ? "active" : "inactive";
-
-    enabledCheck.checked = newValue.enabled;
-
-});
-/************ END presence ************/
 
 
 /************ START selfView  ************/
