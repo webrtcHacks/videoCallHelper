daeaec6cdabc159d1f8f3977dda8879de4133a0c
Chad Hart <chadwhart@gmail.com>
2024-05-27 10:32:41 -0400

    old module removal

---
+++ src/videoPlayer/scripts/base64.mjs
@@ -0,0 +1,30 @@
+
+/**
+ * Helper to convert base64 needed for localStorage to ArrayBuffer
+ * @param {string} base64
+ * @returns {ArrayBuffer}
+ */
+export function base64ToBuffer(base64) {
+    const binaryString = atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+/**
+ * Helper to convert ArrayBuffer to base64 needed for localStorage
+ * @param buffer
+ * @returns {string}
+ */
+export function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
